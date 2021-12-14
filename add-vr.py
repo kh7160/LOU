4ab95c285488e80cff5ff7dfc3b2e8f34a9bb33f
kh7160 <kh7160@naver.com>
2021-12-14 17:47:16 +0900

    vr -ing

--- add-vr.py
+++ add-vr.py
@@ -1,116 +1,103 @@
 import math
 
-import yfinance as yf
+import decimal
 import datetime
 import pandas as pd
+import FinanceDataReader as fdr
 
 pool = 5000
 cnt = 0 # 수량
 v = 0
 week = 0
 tqqq_amt = 0 # 평가금
-sell_loc_list = []
-buy_loc_list = []
+min_v = 0
+max_v = 0
 
 def get_next_inc_rate():
-    if ((pool / v) >= 0.55 and (tqqq_amt >= v)):
-        return 1.06
-    elif ((pool / v) >= 0.5 and (tqqq_amt >= v)) or ((pool / v) >= 0.55 and (tqqq_amt < v)):
-        return 1.055
-    elif ((pool / v) >= 0.45 and (tqqq_amt >= v)) or ((pool / v) >= 0.5 and (tqqq_amt < v)):
-        return 1.05
-    elif ((pool / v) >= 0.4 and (tqqq_amt >= v)) or ((pool / v) >= 0.45 and (tqqq_amt < v)):
-        return 1.045
-    elif ((pool / v) >= 0.35 and (tqqq_amt >= v)) or ((pool / v) >= 0.4 and (tqqq_amt < v)):
-        return 1.04
-    elif ((pool / v) >= 0.3 and (tqqq_amt >= v)) or ((pool / v) >= 0.35 and (tqqq_amt < v)):
-        return 1.035
-    elif ((pool / v) >= 0.25 and (tqqq_amt >= v)) or ((pool / v) >= 0.3 and (tqqq_amt < v)):
-        return 1.03
-    elif ((pool / v) >= 0.2 and (tqqq_amt >= v)) or ((pool / v) >= 0.25 and (tqqq_amt < v)):
-        return 1.025
-    elif ((pool / v) >= 0.15 and (tqqq_amt >= v)) or ((pool / v) >= 0.2 and (tqqq_amt < v)):
-        return 1.02
-    elif ((pool / v) >= 0.1 and (tqqq_amt >= v)) or ((pool / v) >= 0.15 and (tqqq_amt < v)):
-        return 1.015
-    elif ((pool / v) >= 0.05 and (tqqq_amt >= v)) or ((pool / v) >= 0.1 and (tqqq_amt < v)):
-        return 1.01
-    elif ((pool / v) >= 0.01 and (tqqq_amt >= v)) or ((pool / v) >= 0.05 and (tqqq_amt < v)):
-        return 1.005
+    pool_div_v = decimal.Decimal(pool) / decimal.Decimal(v)
+    amt_diff_v = tqqq_amt >= v
+    amt_diff_v_2 = tqqq_amt < v
+
+    if (pool_div_v >= 0.55 and amt_diff_v):
+        return round(decimal.Decimal(1.06), 2)
+    elif (pool_div_v >= decimal.Decimal(0.5) and amt_diff_v) or (pool_div_v >= decimal.Decimal(0.55) and amt_diff_v_2):
+        return round(decimal.Decimal(1.055), 3)
+    elif (pool_div_v >= decimal.Decimal(0.45) and amt_diff_v) or (pool_div_v >= decimal.Decimal(0.5) and amt_diff_v_2):
+        return round(decimal.Decimal(1.05), 2)
+    elif (pool_div_v >= decimal.Decimal(0.4) and amt_diff_v) or (pool_div_v >= decimal.Decimal(0.45) and amt_diff_v_2):
+        return round(decimal.Decimal(1.045), 3)
+    elif (pool_div_v >= decimal.Decimal(0.35) and amt_diff_v) or (pool_div_v >= decimal.Decimal(0.4) and amt_diff_v_2):
+        return round(decimal.Decimal(1.04), 2)
+    elif (pool_div_v >= decimal.Decimal(0.3) and amt_diff_v) or (pool_div_v >= decimal.Decimal(0.35) and amt_diff_v_2):
+        return round(decimal.Decimal(1.035), 3)
+    elif (pool_div_v >= decimal.Decimal(0.25) and amt_diff_v) or (pool_div_v >= decimal.Decimal(0.3) and amt_diff_v_2):
+        return round(decimal.Decimal(1.03), 2)
+    elif (pool_div_v >= decimal.Decimal(0.2) and amt_diff_v) or (pool_div_v >= decimal.Decimal(0.25) and amt_diff_v_2):
+        return round(decimal.Decimal(1.025), 3)
+    elif (pool_div_v >= decimal.Decimal(0.15) and amt_diff_v) or (pool_div_v >= decimal.Decimal(0.2) and amt_diff_v_2):
+        return round(decimal.Decimal(1.02), 2)
+    elif (pool_div_v >= decimal.Decimal(0.1) and amt_diff_v) or (pool_div_v >= decimal.Decimal(0.15) and amt_diff_v_2):
+        return round(decimal.Decimal(1.015), 3)
+    elif (pool_div_v >= decimal.Decimal(0.05) and amt_diff_v) or (pool_div_v >= decimal.Decimal(0.1) and amt_diff_v_2):
+        return round(decimal.Decimal(1.01), 2)
+    elif (pool_div_v >= decimal.Decimal(0.01) and amt_diff_v) or (pool_div_v >= decimal.Decimal(0.05) and amt_diff_v_2):
+        return round(decimal.Decimal(1.005), 3)
     else:
-        return 1.001
+        return round(decimal.Decimal(1.001), 3)
 
 # 적립식 VR
 if __name__ == '__main__':
     target = 'TQQQ'
     add_pool = 250
-    start_date = datetime.datetime(2021, 7, 16)
+    start_date = datetime.datetime(2016, 1, 1)
     end_date = datetime.datetime.now()
-    df = yf.download(target, start=start_date, end=end_date)
+    # end_date = datetime.datetime(2021,10,8)
+    df = fdr.DataReader(symbol='TQQQ', start=start_date, end=end_date)
     df_close_list = df['Close'].values  # 종가
     df_date = pd.to_datetime(df.index, format="%Y-%m-%d")
-    v = pool
-    cnt = math.floor(pool / df_close_list[0]) # 수량
-    tqqq_amt = round((df_close_list[0] * cnt), 4) # 평가금
-    pool = round((pool - tqqq_amt), 4)
-    # 실제 구매 데이터TEST
-    v = 6692.64
-    cnt = 52
-    pool = 1093.72
-    tqqq_amt = round((df_close_list[0] * cnt), 4)  # 평가금
-    ######################################################
-    inc_rate = get_next_inc_rate()
-    print('상승률 : {}'.format(inc_rate))
-    pool += add_pool
-    v = round((v * inc_rate), 2) + add_pool
-    min_v = round((v * 0.8), 2)
-    max_v = round((v * 1.25), 2)
-    for j in range(14):  # rebalance day에 loc 매수, 매도 가격 계산
-        buy_loc_list.append(round(min_v / (cnt + j), 2))
-        sell_loc_list.append(round(max_v / (cnt - j), 2))
-    print('매수loc : {}\n매도loc : {}'.format(buy_loc_list, sell_loc_list))
 
-    print('tqqq수량 : {}, pool : {}, 평가금 : {}'.format(cnt, pool, tqqq_amt))
-    print('v : {}, min_v : {}, max_v : {}'.format(v, min_v, max_v))
-
-    for i in range(1, len(df)):
-        close_price = round(df_close_list[i], 4) # 종가
+    for i in range(len(df)):
+        close_price = round(decimal.Decimal(df_close_list[i]), 2) # 종가
         print('날짜 : {}, 종가 : {}'.format(df_date[i].strftime("%Y%m%d"), close_price))
-        tqqq_amt = round((close_price * cnt), 4)  # 평가금
-        print('tqqq수량 : {}, pool : {}, 평가금 : {}'.format(cnt, pool, tqqq_amt))
         if (i % 10) == 0:
-            week += 2
+            print('rebalance')
             print('{}주차'.format(week))
-            pool += add_pool
-            buy_loc_list.clear()
-            sell_loc_list.clear()
-            print('rebalance day')
-            inc_rate = get_next_inc_rate()
-            print('상승률 : {}'.format(inc_rate))
-            v = round((v * inc_rate), 2) + add_pool
-            min_v = round((v * 0.8), 2)
-            max_v = round((v * 1.25), 2)
-            tqqq_amt = round((close_price * cnt), 4)  # 평가금
-            print('tqqq수량 : {}, pool : {}, 평가금 : {}'.format(cnt, pool, tqqq_amt))
+            if week == 0:
+                v = pool
+                cnt = math.floor(pool / close_price)  # 수량
+                pool -= round(decimal.Decimal(close_price * cnt), 2)
+                inc_rate = get_next_inc_rate()
+                print('상승률 : {}'.format(inc_rate))
+            else:
+                inc_rate = get_next_inc_rate()
+                print('상승률 : {}'.format(inc_rate))
+                v = round((v * inc_rate), 2) + add_pool
+                pool += add_pool
+
+            min_v = round((v * decimal.Decimal(0.8)), 2)
+            max_v = round((v * decimal.Decimal(1.25)), 2)
             print('v : {}, min_v : {}, max_v : {}'.format(v, min_v, max_v))
+            week += 2
 
-            for j in range(14): # rebalance day에 loc 매수, 매도 가격 계산
-                buy_loc_list.append(round(min_v / (cnt + j), 2))
-                sell_loc_list.append(round(max_v / (cnt - j), 2))
-            print('매수loc : {}\n매도loc : {}'.format(buy_loc_list, sell_loc_list))
+        tqqq_amt = round((close_price * cnt), 2)  # 평가금
+        print('tqqq수량 : {}, pool : {}, 평가금 : {}'.format(cnt, pool, tqqq_amt))
 
-        if (close_price < buy_loc_list[0]) and (pool >= close_price): # 종가 < 매수가격 and pool >= 종가 : 1주 매수
-            print('@@@@@@@@@@@@@@@@@@@ 종가({}) < 매수가격({}) and pool >= 종가 @@@@@@@@@@@@@@@@@@@'.format(close_price, buy_loc_list[0]))
-            cnt += 1
-            pool = round((pool - close_price), 4)
-            tqqq_amt = round((close_price * cnt), 4)  # 평가금
-            buy_loc_list.pop(0)
-            # print('tqqq수량 : {}, pool : {}, 평가금 : {}'.format(cnt, pool, tqqq_amt))
+        if tqqq_amt < min_v: # 최소v보다 작을 경우
+            rebl_buy_amt = (pool / decimal.Decimal(2)) if (min_v - tqqq_amt) > (pool / decimal.Decimal(2)) else (min_v - tqqq_amt) # rebalancing 금액(반절 넘지 않게)
+            rebl_buy_cnt = math.ceil(rebl_buy_amt / close_price) # rebalancing용 매수 갯수
+            print(rebl_buy_amt, rebl_buy_cnt)
+            if rebl_buy_cnt > 0 and rebl_buy_amt >= close_price:
+                print('@@@@@@@@@@@@@@@@@ rebalance 매수 @@@@@@@@@@@@@@@@@')
+                pool -= round(rebl_buy_amt, 2)
+                cnt += rebl_buy_cnt
+            print('tqqq수량 : {}, pool : {}, 평가금 : {}'.format(cnt, pool, round((close_price * cnt), 2)))
 
-        if (close_price > sell_loc_list[0]) and (pool < (v * 0.6)): # 종가 > 매도가격 and pool < v*0.6 : 1주 매수
-            print('@@@@@@@@@@@@@@@@@@@ 종가({}) > 매도가격({}) and pool < v*0.6 @@@@@@@@@@@@@@@@@@@'.format(close_price, sell_loc_list[0]))
-            cnt -= 1
-            pool = round((pool + close_price), 4)
-            tqqq_amt = round((close_price * cnt), 4)  # 평가금
-            sell_loc_list.pop(0)
-            # print('tqqq수량 : {}, pool : {}, 평가금 : {}'.format(cnt, pool, tqqq_amt))
+        if tqqq_amt > max_v and pool < (v * decimal.Decimal(0.6)): # 최대v보다 크고 pool이 v*0.6보다 작을 경우
+            rebl_sel_amt = (tqqq_amt - max_v) if (tqqq_amt - max_v + pool) < (v * decimal.Decimal(0.6)) else (v * decimal.Decimal(0.6)) - pool
+            rebl_sel_cnt = math.ceil(rebl_sel_amt / close_price) # rebalancing용 매도 갯수
+            print(rebl_sel_amt, rebl_sel_cnt)
+            if rebl_sel_cnt > 0:
+                print('@@@@@@@@@@@@@@@@@ rebalance 매도 @@@@@@@@@@@@@@@@@')
+                pool += round(rebl_sel_amt, 2)
+                cnt -= rebl_sel_cnt
+            print('tqqq수량 : {}, pool : {}, 평가금 : {}'.format(cnt, pool, round((close_price * cnt), 2)))
